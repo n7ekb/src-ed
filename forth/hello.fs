fdbb99aeeb7e1de41f52c47bfc8da66677c6c91b
Ed Braaten <ed@braaten-family.org>
2015-05-08 23:18:50 -0700

    Added version info output to Hello Forth World script and made initial pass at outputing command line arguments.

--- forth/hello.fs
+++ forth/hello.fs
@@ -3,5 +3,15 @@
 \  Sample Hello World! Forth script...
 \
 
-.( Hello Forth World! ) cr
+: verinfo s" git describe --abbrev=7 --dirty --always --tags" system ;
+
+\ Obligatory Hello World with some version info
+.( Hello Forth World!  Version: ) verinfo cr
+
+\ Show list of command line arguments
+: printargs
+	argc @ 0 do i arg type cr loop ;
+.( Command line args: ) cr
+printargs
+
 bye
